5e141e20c95c860192720a4c7b2d9fb3aa8d8816
Swapnil Opash <sra.opash@gmail.com>
2024-01-01 16:48:31 +0530

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+const url = 'https://api.cryptoconsulting.tube';
+const webUrl = 'https://cryptoconsulting.tube/';
+const tubeUrl = 'https://video.cryptoconsulting.tube/'
+// const url = 'http://localhost:8080';
+// const webUrl = 'http://localhost:4200/';
+
+export const environment = {
+  production: false,
+  hmr: false,
+  serverUrl: `${url}/api/v1/`,
+  socketUrl: `${url}/`,
+  webUrl: webUrl,
+  tubeUrl: tubeUrl,
+  domain: '.cryptoconsulting.tube'
+};
